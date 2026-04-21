e46fbd580788ac10965029fdbe42b82855a6d208
Lakshman kumar <148768096+laksxman@users.noreply.github.com>
2026-04-21 16:18:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://ai-based-exam-notes-client1.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
